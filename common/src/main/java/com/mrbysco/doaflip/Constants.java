bf00919f7ed58a035b34d14a090bb7a138fde6e8
Mrbysco <Mrbysco@users.noreply.github.com>
2025-03-29 01:24:33 +0100

    Port to 1.21.4

--- common/src/main/java/com/mrbysco/doaflip/Constants.java
+++ common/src/main/java/com/mrbysco/doaflip/Constants.java
@@ -1,11 +1,11 @@
 package com.mrbysco.doaflip;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import com.mojang.logging.LogUtils;
+import org.slf4j.Logger;
 
 public class Constants {
 
 	public static final String MOD_ID = "doaflip";
 	public static final String MOD_NAME = "Do A Flip";
-	public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
+	public static final Logger LOGGER = LogUtils.getLogger();
 }
