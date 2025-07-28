7bc4f4de5d38289876f1a23dea37f91666d44289
Mrbysco <Mrbysco@users.noreply.github.com>
2025-07-28 20:32:30 +0200

    Add config option for minimum fall distance

--- forge/src/main/java/com/mrbysco/doaflip/config/ForgeFlipConfig.java
+++ forge/src/main/java/com/mrbysco/doaflip/config/ForgeFlipConfig.java
@@ -13,6 +13,7 @@ import java.util.List;
 public class ForgeFlipConfig {
 	public static class Client {
 		public final ForgeConfigSpec.DoubleValue flipChance;
+		public final ForgeConfigSpec.IntValue minimumFallDistance;
 		public final ForgeConfigSpec.BooleanValue invertMobs;
 		public final ForgeConfigSpec.ConfigValue<List<? extends String>> flippingMobs;
 
@@ -23,6 +24,10 @@ public class ForgeFlipConfig {
 			flipChance = builder
 					.comment("The chance of a flip happening when a mob falls from a high enough distance [0.0 = 0%, 0.1 = 10%, 1.0 = 100%] (Default: 0.35)")
 					.defineInRange("flipChance", 0.35D, 0.0D, 1.0D);
+
+			minimumFallDistance = builder
+					.comment("The minimum fall distance required for a mob to flip (Between 1 and 32) [default: 3]")
+					.defineInRange("minimumFallDistance", 3, 1, 32);
 
 			invertMobs = builder
 					.comment("When enabled turns the flippingMobs option into a blacklist [default: true]")
